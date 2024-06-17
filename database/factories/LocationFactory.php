ba91ba054b140b1015dd015f3e273ecac7e60101
LewBlu <lewis.blundell.development@gmail.com>
2024-06-17 23:23:45 +0100

    Created locations migrations and model

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Location>
+ */
+class LocationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->paragraph(),
+            'lat' => fake()->latitude(),
+            'long' => fake()->longitude(),
+            'owner_id' => User::all()->random()->id,
+        ];
+    }
+}
---
+++ database/migrations/2024_06_16_133339_create_locations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('lat');
+            $table->string('long');
+            $table->foreignId('owner_id')->constrained(table: 'users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
